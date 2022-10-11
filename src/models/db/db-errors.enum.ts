5e1772dbf6fd97030ee694f2e1234bba665acb92
mtxjwo <jennifer@foregate.co>
2022-10-11 21:54:25 +0200

    Update; additional models added.

---
+++ src/models/db/db-errors.enum.ts
@@ -0,0 +1,4 @@
+export enum DBErrorsEnum {
+  INIT_FAILED = "failed to initialise local database.",
+  CONNECT_FAILED = "failed to connect to local database",
+}
---
+++ src/models/ssl/ssl-credentials.model.ts
@@ -0,0 +1,5 @@
+export interface SSLCredentialsModel {
+  key: string;
+  cert: string;
+  ca: string;
+}
